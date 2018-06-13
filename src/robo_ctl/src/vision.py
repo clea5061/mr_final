e151704f50c22834b9fd820d4e56cf04ab7b5daa
Evan Cleary <clea5061@kettering.edu>
2018-06-13 12:06:52 -0400

    Fix divide by zero issue

--- src/robo_ctl/src/vision.py
+++ src/robo_ctl/src/vision.py
@@ -4,6 +4,11 @@ from geometry_msgs.msg import Twist
 
 def lane_center_to_twist(center, speed):
     twst = Twist()
+    twst.linear.y = speed
+    if center == 0:
+        twst.angular.x = speed
+        twst.angular.y = speed
+        return twst
     z = 33.0
     curve = sqrt(center**2 + z**2)/(2*sin(np.pi/(1+center/160))*23.0)
     velocity = speed+(speed-(2*speed*curve))/(2*curve+1)
